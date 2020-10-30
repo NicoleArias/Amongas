e15770c260f1dbbf173263e6dcbae227564f68e8
NicoleArias <ariasnicole689@gmail.com>
2020-10-30 11:21:35 -0500

    Diseño

---
+++ Vista/js/tarea_medium_reactor.js
@@ -0,0 +1,52 @@
+
+// function xd(){
+
+    // Array.prototype.mezclar= function() {
+    //     var n=this.length;
+    //     while(n--)
+    //     {
+    //         var i=Math.floor(n*Math.random());
+    //         var tmp =this[i];
+    //         this[i]=this[n];
+    //         this[n]=tmp;
+    //     }
+    //     return this;
+    // }
+     
+     
+    // //Luego donde quieras
+    // var miArray=[1, 3, 4, 5, 6, 7, 8, 9, 2, 10 ];
+    // miArray.mezclar();
+
+
+
+    // Array.prototype.shuffle = function() {
+    //     for ( var i = this.length-1; i > 0; i-- ) {
+    //          var tmp = this[ j ];
+    //         this[ j ] = this[ i ];
+    //         this[ i ] = tmp;
+    //     }
+    //     return this;
+    // }
+    // var foo = [1, 3, 4, 5, 6, 7, 8, 9, 2, 10 ];
+    
+    // console.log( foo.shuffle() );
+
+    // var number = new Array(foo.shuffle());
+    // console.log(number);
+
+    // document.getElementById('posicion_1').innerHTML = foo.shuffle();
+// }
+
+
+
+
+
+function xd(){
+   
+            console.log('naa');
+  
+}
+
+
+
